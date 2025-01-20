70cbddfd848491704839cf18565df7f6508a7185
Ashen Thilakarathna <134621843+Ashein00@users.noreply.github.com>
2025-01-20 12:12:13 +0530

    Firebase db connection is tested!

---
+++ firebase.ts
@@ -0,0 +1,13 @@
+import { initializeApp, getApps, getApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBPTLEU-nTKJot5B77RqyFE1x8TmRq2WEo",
+  authDomain: "notion-clone-17dc8.firebaseapp.com",
+  projectId: "notion-clone-17dc8",
+  storageBucket: "notion-clone-17dc8.firebasestorage.app",
+  messagingSenderId: "75092662824",
+  appId: "1:75092662824:web:935d83d3905bf80a1b331d"
+};
+
+const app = getApps.length === 0 ? initializeApp(firebaseConfig) : getApp();
